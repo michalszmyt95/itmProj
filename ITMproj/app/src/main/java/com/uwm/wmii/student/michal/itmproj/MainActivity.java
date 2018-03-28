3a49aed750c42c24044c995a185f2bc23407d885
BK <bartozo.krol@gmail.com>
2018-03-28 18:54:32 +0200

    Zmiany w UI

--- ITMproj/app/src/main/java/com/uwm/wmii/student/michal/itmproj/MainActivity.java
+++ ITMproj/app/src/main/java/com/uwm/wmii/student/michal/itmproj/MainActivity.java
@@ -1,12 +1,17 @@
 package com.uwm.wmii.student.michal.itmproj;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -17,7 +22,7 @@ import com.uwm.wmii.student.michal.itmproj.singletons.AppLoginManager;
 public class MainActivity extends AppCompatActivity {
 
     private AppLoginManager appLoginManager;
-
+    private DrawerLayout mDrawerLayout;
     Button testyBtn;
     Button testyBtn2;
 
@@ -26,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.appLoginManager = AppLoginManager.getInstance(getApplicationContext());
-
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         ustawPrzyciski();
     }
 
@@ -40,20 +50,31 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) { // ustawienie logout buttonu
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return  true;
             case R.id.logout:
                 appLoginManager.wyloguj();
                 super.onBackPressed(); // powrót do poprzedniej aktywności.
                 break;
+            case R.id.nav_profile:
+                Toast.makeText(this,"Profil",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_party:
+                Toast.makeText(this,"Imprezy",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_history:
+                Toast.makeText(this,"Historia",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.nav_logout:
+                Toast.makeText(this,"Wyloguj",Toast.LENGTH_SHORT).show();
+                return true;
         }
-
-        return true;
+        return super.onOptionsItemSelected(item);
     }
-
     private void ustawPrzyciski() {
        testyBtn = findViewById(R.id.testyBtn);
         testyBtn.setOnClickListener(new View.OnClickListener() {
