295c4a47a88b0d11b0f1c6303c860eff3a8a2505
michalszmyt95 <michalszmyt95@gmail.com>
2018-04-17 22:53:57 +0200

    Usunięcie niepotrzebnych plików.

--- ITMproj/app/src/main/java/com/uwm/wmii/student/michal/itmproj/Fragments/ProfileFragment.java
+++ ITMproj/app/src/main/java/com/uwm/wmii/student/michal/itmproj/Fragments/ProfileFragment.java
@@ -10,26 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.support.v7.app.AppCompatActivity;
 
 import com.uwm.wmii.student.michal.itmproj.R;
-import com.uwm.wmii.student.michal.itmproj.UserDataActivity;
 import com.uwm.wmii.student.michal.itmproj.api.dto.ProfilDTO;
-import com.uwm.wmii.student.michal.itmproj.api.dto.UserDTO;
 import com.uwm.wmii.student.michal.itmproj.api.dto.WynikOperacjiDTO;
 import com.uwm.wmii.student.michal.itmproj.api.service.UserRestService;
 import com.uwm.wmii.student.michal.itmproj.singletons.AppLoginManager;
 import com.uwm.wmii.student.michal.itmproj.singletons.AppRestManager;
 
-import org.w3c.dom.Text;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * A simple {@link Fragment} subclass.
